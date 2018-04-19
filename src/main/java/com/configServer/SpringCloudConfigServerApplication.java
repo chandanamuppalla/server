f3e8c7b1694878325279c155ef41d3cc8c636861
cmuppalla.5@gmail.com <cmuppalla.5@gmail.com>
2018-04-19 14:23:14 -0400

    serverapp

---
+++ src/main/java/com/configServer/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.configServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+	
+}
